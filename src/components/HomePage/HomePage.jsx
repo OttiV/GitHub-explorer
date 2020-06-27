c9e2c693a221f523bd970fa3b76e034f2c57762b
Vgnttv <o.vignani@gmail.com>
2020-06-27 20:07:50 +0200

    Add pagination to repos displayed

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,13 +1,15 @@
 import React, { lazy, Suspense, useEffect, useState } from "react";
-import { SearchBox } from "./components";
+import { SearchBox, Pagination } from "./components";
 import "./HomePage.css";
 const ReposList = lazy(() => import("./components/ReposList"));
 
 function HomePage() {
-  const [repos, setRepos] = useState("");
+  const [repos, setRepos] = useState([]);
   const [search, setSearch] = useState("");
   const [time, setTime] = useState("");
   const [error, setError] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [reposPerPage] = useState(10);
   useEffect(() => {
     const startTime = new Date().getTime();
     fetch("https://api.github.com/repositories")
@@ -27,17 +29,28 @@ function HomePage() {
   const handleInput = event => {
     setSearch(event.target.value);
   };
+  const indexOfLastRepo = currentPage * reposPerPage;
+  const indexOfFirstRepo = indexOfLastRepo - reposPerPage;
+  const currentRepos = repos.slice(indexOfFirstRepo, indexOfLastRepo);
+
   const filteredRepos =
     repos &&
-    repos.filter(repo =>
+    currentRepos.filter(repo =>
       repo.name.toLowerCase().includes(search.toLowerCase())
     );
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
     <div className="container">
       <SearchBox search={search} handleInput={handleInput} time={time} />
       <Suspense fallback={<div>Loading...</div>}>
         <ReposList repos={filteredRepos} error={error} />
+        <Pagination
+          reposPerPage={reposPerPage}
+          totalRepos={repos.length}
+          paginate={paginate}
+        />
       </Suspense>
     </div>
   );
---
+++ src/components/HomePage/components/Pagination.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Pagination.css"
+ 
+const Pagination = ({ reposPerPage, totalRepos, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalRepos / reposPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <div className="pagination">
+      {pageNumbers.map(number => (
+        <div key={number} className="button" onClick={() => paginate(number)}>
+            {number}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Pagination;

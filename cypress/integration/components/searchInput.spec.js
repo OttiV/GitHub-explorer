f20b38b812ca9abd27882cc87c849ff3afa31fba
Vgnttv <o.vignani@gmail.com>
2020-06-29 16:20:51 +0200

    Implement test for search input

--- cypress/integration/components/searchInput.spec.js
+++ cypress/integration/components/searchInput.spec.js
@@ -1,30 +1,34 @@
 import { baseUrl } from "../constants";
 import { mockGetReposList } from "../../mock/repos_mocks";
+import { repoLine, searchInput } from "../selectors";
 
-const reposList = "[data-cy=reposList]";
-const repoLine = "[data-cy=repoLine]";
-
-const getRepoId = num => {
-  // index of repo + 1 is id of repo
-  return `[data-cy=repoDetails-${num + 1}]`;
+const checkFilteredRepos = (num, text) => {
+  cy.get(repoLine)
+    .eq(num)
+    .contains(text);
 };
+const indexOfFilteredRepos = [0, 1, 2, 3];
+const text = "go";
 
-// describe("Search input test cases", function() {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000/");
-//     cy.server();
-//     mockGetReposList(true);
-//   });
-//   it("Should display list", function() {
-//     cy.get(reposList)
-//       .children(repoLine)
-//       .eq(9)
-//       .should("exist");
-//   });
-//   it("Should display repo details after click on repo line", function() {
-//     cy.get(repoLine)
-//       .eq(0) // index of repo
-//       .click();
-//     cy.get(getRepoId(0));
-//   });
-// });
+describe("Search input test cases", function() {
+  beforeEach(() => {
+    cy.visit(baseUrl);
+    cy.server();
+    mockGetReposList(true);
+  });
+  it("Should display search input", function() {
+    cy.get(searchInput).should("exist");
+  });
+  it("Should filter repos when user types", function() {
+    cy.get(searchInput)
+      .type(text)
+      .should("have.value", text);
+    cy.get(repoLine)
+      .eq(3)
+      .should("exist");
+    cy.get(repoLine)
+      .eq(4)
+      .should("not.exist");
+    indexOfFilteredRepos.forEach(index => checkFilteredRepos(index, text));
+  });
+});

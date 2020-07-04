ec77c392482a794dce48ee92f8f019eedf1e50bb
Vgnttv <o.vignani@gmail.com>
2020-07-04 20:13:31 +0200

    Improve pagination style

--- src/components/RepoList/components/Pagination.jsx
+++ src/components/RepoList/components/Pagination.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "./Pagination.css";
 
-const Pagination = ({ reposPerPage, totalRepos, paginate }) => {
+const Pagination = ({ reposPerPage, totalRepos, paginate, currentPage }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalRepos / reposPerPage); i++) {
     pageNumbers.push(i);
   }
   return (
     <div className="pagination" data-cy="pagination">
-      {pageNumbers.map(number => (
-        <div
-          key={number}
-          className="button"
-          data-cy="paginationButton"
-          onClick={() => paginate(number)}
-        >
-          {number}
-        </div>
-      ))}
+      <div className="buttonWrapper">
+        {pageNumbers.map(number => (
+          <div
+            key={number}
+            className={`button ${currentPage === number && "currentPage"}`}
+            data-cy="paginationButton"
+            onClick={() => paginate(number)}
+          >
+            {number}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
